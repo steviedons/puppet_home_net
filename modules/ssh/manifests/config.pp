f5df2ea3d3784e36980e56211c5f6529cafd17ff
Stephen Donovan <steviedons@gmail.com>
2015-01-29 17:14:55 +0000

    changed the source for the key files in ssh:config class

--- modules/ssh/manifests/config.pp
+++ modules/ssh/manifests/config.pp
@@ -11,15 +11,15 @@ class ssh::config($sshd_config_source = "puppet:///modules/ssh/etc/ssh/sshd_conf
     owner   => "root",
     group   => "root",
     mode    => 0600,
-    source  => "puppet:///private/etc/ssh/ssh_host_rsa_key",
+    source  => "puppet:///modules/ssh/ssh_host_rsa_key",
     require => Class["ssh::install"],
     notify  => Service["ssh"],
   }
-  file { "/etc/ssh/.ssh/id_rsa.pub":
+  file { "/home/steve/.ssh/id_rsa.pub":
     owner   => "root",
     group   => "root",
     mode    => 0644,
-    source  => "puppet:///private/etc/ssh/ssh_host_rsa_key.pub",
+    source  => "puppet:///modules/ssh/ssh_host_rsa_key.pub",
     require => Class["ssh::install"],
     notify  => Service["ssh"],
   }
