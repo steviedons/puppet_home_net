a2d88a158d43450b2d0027231f18a03af167143e
Stephen Donovan <steviedons@gmail.com>
2015-01-29 15:25:17 +0000

    removed nodes.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,12 +10,12 @@ class base {
                 type => 'rsa',
                 key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1Qumt9tDkO0EYLwfb1x/HXFHG+CPSfsAOnOItZQ4JtRYozoZI1408deSOUfLp4P14aBOaOj3FclPYMBYJS5Kzoem59v3ycRspS0cZVK2Zsq6z2vRcodUJ/8nNPykd1zGlftFzFXiQ9/RyDV/TGwRqmhHiIJE7gPiMBU7ihPCOFh+h8TYDvxoln+yyBpzOWj2WpZ8DwIfeWLZG3NlEeKScpoBtIz206sFcZ6cn2goYbQQGGVErIs/gBV6W3gi4yOybtxnX5CYQHCTI4d9i0NL000Oo5aw6GuHbtNDk2dt1cCj8QWMSfSO9qasP9qBwy6afG+PZOGERy/WoSLgxpZi/',
         }
-        cron { 'Back up system':
-                command => 'rsync -az --exclude-from=/home/steve/exclude_file.txt / /media/backup/calisto-backup',
-                user => root,
-                hour => '3',
-                minute  => '00',
-        }
+#        cron { 'Back up system':
+#                command => 'rsync -az --exclude-from=/home/steve/exclude_file.txt / /media/backup/calisto-backup',
+#                user => root,
+#                hour => '3',
+#                minute  => '00',
+#        }
 }
 
 
@@ -34,4 +34,7 @@ node 'metis.steviedons.com' {
         include base
 	include hosts
         include resolv
+}
+node 'mate.steviedons.com' {
+	include base
 }
