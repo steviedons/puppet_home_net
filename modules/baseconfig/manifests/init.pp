c8d54eb7370ced11dc0620d18bd6b36f55c65fe3
Stephen Donovan <steve@mimas.steviedons.com>
2016-04-14 22:07:32 +0100

    updated the baseconfig to fix errors and run on mimas without issues

--- modules/baseconfig/manifests/init.pp
+++ modules/baseconfig/manifests/init.pp
@@ -4,25 +4,26 @@
 #
 class baseconfig {
   
-  package { ['epel-release', 'vim-enhanced', 'htop', 'tree', 'unzip', 'git', 'docker']:
+  package { ['epel-release', 'yum-cron', 'vim-enhanced', 'htop', 'tree', 'unzip', 'git', 'docker']:
     ensure => present,
   }
 
   group { 'wheel':
     ensure => present,
+  }
 
   user { 'steve':
     ensure      => present,
     comment     => 'Stephen Donovan',
     home        => '/home/steve',
-    groups      => 'wheel'
+    groups      => 'wheel',
     managehome  => true,
   }
 
   ssh_authorized_key { 'steve_ssh':
     user    => 'steve',
     type    => 'rsa',
-    key     => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCY7dN61s6H57fYGV2THhvTzhi0xXQLd59UTSmKc8D+Vl7v7OzBFNL43VfdzsXk539ylo6jrWjeI4pmIL5oBRBIf/X57DW/c33TBYhePfEJC7XiQLibWpgo0dz4uoa0iFBGE10UymoNMsuGYIFnzXF0UXQ7LlT/f1rdZp3iv7P0B4rU5HtxLI30eAlXvG62ODxSXCn7xvsrkvR7PHC/LPxKe3vqQfwoQ2qg8F1jXbtNKQtQuDaT/PdvkjkhD/KM+YQH9sLuYyngaTSjbu6BMpMWaRwlvvM0byCbKd+gzhXJohENHm8+qsSjNjZjbyFUoyYthwBC8ER2WHLNe4tj2u9N steve@titan',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCY7dN61s6H57fYGV2THhvTzhi0xXQLd59UTSmKc8D+Vl7v7OzBFNL43VfdzsXk539ylo6jrWjeI4pmIL5oBRBIf/X57DW/c33TBYhePfEJC7XiQLibWpgo0dz4uoa0iFBGE10UymoNMsuGYIFnzXF0UXQ7LlT/f1rdZp3iv7P0B4rU5HtxLI30eAlXvG62ODxSXCn7xvsrkvR7PHC/LPxKe3vqQfwoQ2qg8F1jXbtNKQtQuDaT/PdvkjkhD/KM+YQH9sLuYyngaTSjbu6BMpMWaRwlvvM0byCbKd+gzhXJohENHm8+qsSjNjZjbyFUoyYthwBC8ER2WHLNe4tj2u9N',
     require => User['steve'],
   }
 
