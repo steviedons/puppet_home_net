8740d2cd555cba0ff2770c031500a4a1d738e85f
Stephen Donovan <steviedons@gmail.com>
2016-04-14 23:15:08 +0100

    adding the docker group to user steve

--- modules/baseconfig/manifests/init.pp
+++ modules/baseconfig/manifests/init.pp
@@ -10,13 +10,14 @@ class baseconfig {
 
   group { 'wheel':
     ensure => present,
-
+  }
   user { 'steve':
-    ensure      => present,
-    comment     => 'Stephen Donovan',
-    home        => '/home/steve',
-    groups      => 'wheel'
-    managehome  => true,
+    ensure     => present,
+    comment    => 'Stephen Donovan',
+    home       => '/home/steve',
+    groups     => ['wheel','docker'],
+    password   => '$6$9oEtZVi1$MEHcx4PbHsu81cDb2EJPElBOMHDQkIg4Bjn.hifQ8aFVnT9tFrVqbvJXl6DJ5ncJC0m5wuzMf61u/qtDXmp291'
+    managehome => true,
   }
 
   ssh_authorized_key { 'steve_ssh':
